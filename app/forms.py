3544deb28f80e0da9f79e3ea323a8f17a9889c53
varunvaruns9 <varunsinghs2021@gmail.com>
2019-04-06 17:43:34 +0530

    Add signup view.

---
+++ app/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from .choices import Organs, Groups, RH
+from multiselectfield import MultiSelectField
+
+class SignUpForm(UserCreationForm):
+	first_name = forms.CharField(max_length=30)
+	last_name = forms.CharField(max_length=30)
+	email = forms.EmailField(max_length=254)
+	donor = forms.BooleanField(initial=False)
+	organs = MultiSelectField(choices=Organs, label="Organs")
+	bloodgroup = forms.ChoiceField(choices=Groups, label="BloodGroup")
+	rh = forms.ChoiceField(choices=RH, label="RH")
+	ailment = forms.BooleanField(initial=False)
+	report = forms.FileField()
+
+	class Meta:
+		model = User
+		fields = ['email', 'first_name', 'last_name', 'donor', 'organ', 'bloodgroup', 'rh', 'ailment', 'report', 'password1', 'password2']
--- app/views.py
+++ app/views.py
@@ -1,18 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .algo import *
 from .models import User
 from .choices import Organs
+from .forms import SignUpForm
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import AuthenticationForm as LoginForm
+from django.contrib.auth import login, authenticate
 
 
 def index(request):
-	lol = []
-	for row in User.objects.all():
-		print(row.organs)
-		if row.donor == True:
-			for a in row.organs:
-				lol.append({'name': row.first_name + ' ' + row.last_name, 'organ': Organs[int(a)][1], 'email': row.email, })
-	return render(request, 'index.html', {'lol': lol})
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('results/')
+	else:
+		error=""
+		lol = []
+		for row in User.objects.all():
+			print(row.organs)
+			if row.donor == True:
+				for a in row.organs:
+					lol.append({'name': row.first_name + ' ' + row.last_name, 'organ': Organs[int(a)][1], 'email': row.email, })
+		if request.method == 'POST':
+			form = LoginForm(request.POST)
+			username = request.POST['username']
+			password = request.POST['password']
+			user = authenticate(request, username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return HttpResponseRedirect('results/')
+			else:
+				error+="Invalid details."
+		else:
+			form = LoginForm()
+		return render(request, 'index.html', {'form': form, 'error': error, 'lol': lol})
 
+def signup(request):
+	if request.user.is_authenticated:
+		return HttpResponseRedirect('results/')
+	else:
+		error=""
+		lol = []
+		for row in User.objects.all():
+			print(row.organs)
+			if row.donor == True:
+				for a in row.organs:
+					lol.append({'name': row.first_name + ' ' + row.last_name, 'organ': Organs[int(a)][1], 'email': row.email, })
+		if request.method == 'POST':
+			form = SignUpForm(request.POST)
+			if not form.is_valid():
+				error += "Invalid information/ Email already in use."
+			else:
+				user = form.save(commit=False)
+				user.save()
+		else:
+			form = SignUpForm()
+		return render(request, 'register.html', {'form': form, 'error': error, 'lol': lol})
 
 def results(request):
 	lista, listb = stable_match()
